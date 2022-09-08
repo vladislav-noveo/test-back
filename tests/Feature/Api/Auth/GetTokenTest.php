9fa653934c83ecb5dd7a57c8d527aa858bb0613b
Vladislav Zherdev <vladislav.zherdev@noveogroup.com>
2022-09-08 10:50:04 +0300

    Returned test

--- tests/Feature/Api/Auth/GetTokenTest.php
+++ tests/Feature/Api/Auth/GetTokenTest.php
@@ -8,25 +8,25 @@ use Illuminate\Support\Facades\Hash;
 
 class GetTokenTest extends ApiTestCase
 {
-    // public function testUserCanGetToken()
-    // {
-    //     $user = User::factory([
-    //         'email' => 'test@email.com',
-    //         'password' => Hash::make('testPass'),
-    //     ])->create();
+    public function testUserCanGetToken()
+    {
+        $user = User::factory([
+            'email' => 'test@email.com',
+            'password' => Hash::make('testPass'),
+        ])->create();
 
-    //     $response = $this->post(route('token.get'), [
-    //         'email' => $user->email,
-    //         'password' => 'testPass',
-    //         'device_name' => 'test',
-    //     ]);
+        $response = $this->post(route('token.get'), [
+            'email' => $user->email,
+            'password' => 'testPass',
+            'device_name' => 'test',
+        ]);
 
-    //     $response->assertSuccessful();
-    //     $token = $response->getContent();
+        $response->assertSuccessful();
+        $token = $response->getContent();
 
-    //     $response = $this->getJson(route('bookings.get'), ['Authorization' => 'Bearer ' . $token]);
-    //     $response->assertSuccessful();
-    // }
+        $response = $this->getJson(route('bookings.get'), ['Authorization' => 'Bearer ' . $token]);
+        $response->assertSuccessful();
+    }
 
     /**
      * @dataProvider invalidCredentialsProvider
